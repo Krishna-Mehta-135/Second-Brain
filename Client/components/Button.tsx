3cdf2e26a7bc9a8a784a51003f83d32de27fa787
Krishna Mehta <krishnamehta135@protonmail.com>
2025-04-30 01:45:16 +0530

    Added button ui lib

--- Client/components/Button.tsx
+++ Client/components/Button.tsx
@@ -1,16 +1,34 @@
-import { ReactElement } from 'react';
+import {ReactElement} from "react";
 
-type Variants = "Primary" | "Secondary"
+type Variants = "Primary" | "Secondary";
 
 interface ButtonProps {
-    variant : Variants;
-    size : "sm" | "md" | "lg";
-    text : string;
-    startIcon ?: ReactElement;
-    endIcon ?: ReactElement;
-    onClick : () => void;
+    variant: Variants;
+    size: "sm" | "md" | "lg";
+    text: string;
+    startIcon?: ReactElement;
+    endIcon?: ReactElement;
+    onClick: () => void;
 }
 
 const variantStyles = {
-    // "Primary" : 
-}
+    Primary: "bg-purple-600 text-white",
+    Secondary: "bg-purple-300 text-purple-500",
+};
+
+const defaultStyles = "rounded-md flex items-center";
+const sizeStyles = {
+    sm: "py-1 px-2",
+    md: "py-2 px-4",
+    lg: "py-4 px-6",
+};
+
+export const Button = (props: ButtonProps) => {
+    return (
+        <button className={` ${variantStyles[props.variant]} ${defaultStyles} ${sizeStyles[props.size]}`}>
+            {props.startIcon ? <div className="pr-2">{props.startIcon}</div> : null}
+            {props.text}
+            {props.startIcon ? <div className="pl-2">{props.endIcon}</div> : null}
+        </button>
+    );
+};
