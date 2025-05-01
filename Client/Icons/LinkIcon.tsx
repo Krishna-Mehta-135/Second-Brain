9c353c3775ab8c16ef2ae3eae91ccf7bec541517
Krishna Mehta <krishnamehta135@protonmail.com>
2025-05-02 03:51:03 +0530

    Added card component

---
+++ Client/Icons/LinkIcon.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const LinkIcon = () => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 10.5l-3 3m0 0l-3-3m3 3V3m6 6h3.75A2.25 2.25 0 0121 11.25v9A2.25 2.25 0 0118.75 21h-13.5A2.25 2.25 0 013 18.75v-9A2.25 2.25 0 015.25 9H9"
+            />
+        </svg>
+    );
+};
